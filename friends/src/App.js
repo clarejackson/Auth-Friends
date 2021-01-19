2bd8e7a7e9640fac6804d1f303dd616d38e658fa
Clare Jackson <clare-jackson@lambdastudents.com>
2021-01-18 19:45:01 -0500

    styling done on Navbar

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,33 @@
+import "./App.css" 
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { PrivateRoute } from "./components/PrivateRoute";
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import NewFriend from './components/NewFriend';
-import { Nav, NavItem, NavLink } from "reactstrap"
+import { Navbar, Nav, Button, Container } from "react-bootstrap"
 
-function App() {
+function App(props) {
+  const logout = () => {
+    localStorage.removeItem("token");
+  }
+
   return (
     <div className="App">
       <Router>
-      <nav>
-          <Link to="/login">Login</Link>
-          <Link>Logout</Link>
-          <Link>Protected Page</Link>
-      </nav>
+        <Container>
+      <Navbar className="nav" variant="dark" expand="lg" style={{ backgroundColor: '#F64C3C' }}>
+      <Navbar.Brand className="brand" style={{ color: "black" }}>Find Friends!</Navbar.Brand>
+        <Navbar.Collapse className="nav-container" id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+          <Nav.Link className="nav-link" href="/login">Login</Nav.Link>
+          <Nav.Link className="nav-link" onClick={logout}>Logout</Nav.Link>
+          <Nav.Link className="nav-link">Protected Page</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+          </Navbar>
+      </Container>
+
       <Switch>
       <PrivateRoute exact path="/protected">
         <NewFriend />
@@ -23,6 +36,7 @@ function App() {
       <Route exact path="/login" component={Login} />
         <Route component={Login} />
       </Switch>
+
       </Router>
     </div>
   );
